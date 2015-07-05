716a6307dba48e7c23d9d13f61e99d922d9b35d7
rajatshetty <rshetty47@gmail.com>
2015-07-05 22:20:42 +0530

    Checking in after creating different controllers and adding a seperate factory.

--- js/app.js
+++ js/app.js
@@ -12,11 +12,11 @@ angular.module("app", []).config(["$routeProvider",function($routeProvider) {
         }).
         when('/page1', {
             templateUrl: 'htmls/page1.html',
-            controller: initController
+            controller: page1Controller
         }).
         when('/page2', {
             templateUrl: 'htmls/page2.html',
-            controller: initController
+            controller: page2Controller
         }).
         otherwise({
             redirectTo: '/welcome'
@@ -30,6 +30,22 @@ angular.module("app", []).config(["$routeProvider",function($routeProvider) {
 });
 
 
-angular.modue("app")
+angular.module("app").factory("DataStore",['$http',function($http){
+
+    this.getWelcomePageData=function(){
+    return "Data for welcome page from factory";
+    }
+
+    this.getPage1Data=function(){
+      return "Data for Page 1 from factory";
+    }
+
+    this.getPage2Data=function(){
+       return "Data for Page 2 from factory";
+
+    }
+
+    return this;
+}]);
 
 
--- js/controller.js
+++ js/controller.js
@@ -2,9 +2,62 @@
  * Created by rshetty2 on 7/5/15.
  */
 'use strict';
-function initController($scope,$routeParams){
+function initController($scope,$routeParams,$location,DataStore) {
 
+    $scope.initPage=function()
+    {
+        var uri = $location.path();
+        if(uri=='/page1'){
+            $scope.data=DataStore.getPage1Data();
+        }
+        else if(uri=='/page2'){
+            $scope.data=DataStore.getPage2Data();
+        }
+        else
+        {
+            $scope.data=DataStore.getWelcomePageData();
+        }
 
+    }
 
+}
+
+function page1Controller($scope,$routeParams,$location,DataStore) {
+
+    $scope.initPage=function()
+    {
+        var uri = $location.path();
+        if(uri=='/page1'){
+            $scope.data=DataStore.getPage1Data();
+        }
+        else if(uri=='/page2'){
+            $scope.data=DataStore.getPage2Data();
+        }
+        else
+        {
+            $scope.data=DataStore.getWelcomePageData();
+        }
+
+    }
+
+}
+
+function page2Controller($scope,$routeParams,$location,DataStore) {
+
+    $scope.initPage=function()
+    {
+        var uri = $location.path();
+        if(uri=='/page1'){
+            $scope.data=DataStore.getPage1Data();
+        }
+        else if(uri=='/page2'){
+            $scope.data=DataStore.getPage2Data();
+        }
+        else
+        {
+            $scope.data=DataStore.getWelcomePageData();
+        }
+
+    }
 
 }
